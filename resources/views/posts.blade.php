059004f357e99103c8c90f83977c41aa096a6ed7
mikedevilee <6010768@mborijnland.nl>
2023-01-22 19:48:52 +0100

    Eerste en gelijk complete commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+@foreach ($posts as $post)
+    <article>
+        <h1>
+            <a href="/posts/{{$post->id}}">
+                {{$post->title}}
+            </a>
+        </h1>
+        <div>
+            {{$post->excerpt}}
+        </div>
+    </article>
+
+
+@endforeach
